777168b3d73b8b61df404093c233fb4b71714d43
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-11-10 14:52:11 -0300

    Reset

--- resources/views/filament/logo.blade.php
+++
@@ -1,6 +0,0 @@
-<![CDATA[<div class="flex items-center gap-2 transition-colors">
-    <!-- You can replace this with an SVG or image -->
-    <span class="text-2xl font-bold tracking-tight text-primary-600 dark:text-primary-400">
-        URL
-    </span>
-</div>]]>
